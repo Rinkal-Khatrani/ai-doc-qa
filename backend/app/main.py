da5712e899ef5af4e8cc274ace8ac48eec80d65b
Rinkal Khatrani <rinkalkhatrani123@gmail.com>
2026-06-01 11:38:39 +0530

    cors policy

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,8 @@ app = FastAPI(title="Doc Q&A API", lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[ "http://localhost:5173",
-        "http://127.0.0.1:5173",],
+        "http://127.0.0.1:5173",
+        "https://ai-doc-qa-production-de49.up.railway.app"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allow_headers=["*"],
